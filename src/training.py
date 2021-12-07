91370bd2eb5c09aeb8003b04f7106cbfd19ca5ba
iambalakrishnan <impythonprogrammer@gmail.com>
2021-12-07 22:38:54 +0530

    get data function implemented

--- src/training.py
+++ src/training.py
@@ -1,5 +1,7 @@
 from src.utils.common import read_config
+from tensorflow.python.ops.gen_math_ops import HistogramFixedWidth
 from src.utils.data_management import get_data
+from src.utils.model import create_model
 import argparse
 
 def training(config_path):
@@ -9,6 +11,19 @@ def training(config_path):
 
     (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
 
+    LOSS_FUNCTION =  config["params"]["loss_function"]
+    OPTIMIZER = config["params"]["optimizer"]
+    METRICS = config["params"]["metrics"]
+    NUM_CLASSES = config["params"]["num_classes"]
+
+    model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+
+    EPOCHS = config["params"]["epochs"]
+    VALIDATION_SET = ( X_valid, y_valid )
+
+    history = model.fit(X_train, y_train, 
+                        epochs=EPOCHS, 
+                        validation_data=VALIDATION_SET)
     
     
 
