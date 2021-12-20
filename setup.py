077e31e25f79449509fa85eb3904ca752395f3b9
iambalakrishnan <impythonprogrammer@gmail.com>
2021-12-20 16:50:37 +0530

    callbacks

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="src",
-    version="0.0.1",
+    version="0.0.2",
     author="iambalakrishnan",
     description="A small package for ANN implementation",
     long_description=long_description,
