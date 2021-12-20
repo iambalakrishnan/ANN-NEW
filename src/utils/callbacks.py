89422ebdfcc2e7028d9bf4d2fb0daaf276c1a3e8
iambalakrishnan <impythonprogrammer@gmail.com>
2021-12-20 16:48:25 +0530

    call backs implemented

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -1,4 +1,44 @@
+import tensorflow as tf
+import numpy as np
+import os
+import time
 
+
+
+
+def get_timestamp(name):
+    timestamp = time.asctime().replace(" ", "_").replace(":", "_")
+    unique_name = f"{name}_at_{timestamp}"
+    return unique_name
 
 def get_callbacks(config, X_train):
-    pass
+    logs = config["logs"]
+    unique_dir_name = get_timestamp("tb_logs")
+    TENSORBOARD_ROOT_LOG_DIR = os.path.join(logs["logs_dir"], logs["TENSORBOARD_ROOT_LOG_DIR"],unique_dir_name)
+
+    os.makedirs(TENSORBOARD_ROOT_LOG_DIR, exist_ok=True)
+    #TENSORBOARD CALLBACKS
+    tensorboard_cb = tf.keras.callbacks.TensorBoard(log_dir=TENSORBOARD_ROOT_LOG_DIR)
+
+    file_writer = tf.summary.create_file_writer(logdir=TENSORBOARD_ROOT_LOG_DIR)
+    with file_writer.as_default():
+        images = np.reshape(X_train[10:30], (-1, 28, 28, 1)) ###<<< 20- images, 28, 28 - size, 1 -channel i.e black and white channel
+        tf.summary.image("20 handwritten digit samples ", images, max_outputs=25, step=0)
+    
+    #Early stopping callback
+
+    params = config["params"]
+    early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience=params["patience"], 
+                                            restore_best_weights= params["restore_best_weights"])
+
+    #Model checkpoint callback
+    artifacts = config["artifacts"]
+
+    CKPT_dir = os.path.join(artifacts["artifacts_dir"], artifacts["CHECKPOINT_DIR"])
+    os.makedirs(CKPT_dir, exist_ok=True)
+
+    CKPT_path = os.path.join(CKPT_dir, "model_ckpt.h5")
+
+    checkpointing_cb = tf.keras.callbacks.ModelCheckpoint(CKPT_path, save_best_only=True)
+
+    return [tensorboard_cb, early_stopping_cb, checkpointing_cb]
--- src/utils/common.py
+++ src/utils/common.py
@@ -1,8 +1,13 @@
 import yaml
+import os
+import time
 
 def read_config(config_path):
     with open(config_path) as config_file:
         content = yaml.safe_load(config_file)
 
     return content
+
+
+
     
